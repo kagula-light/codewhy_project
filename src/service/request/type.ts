c13da5baa1ca68b181980a9b07fa01c4d5612e4a
付恒吉 <649942163@qq.com>
2023-06-28 01:48:42 +0800

    微调

--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -9,6 +9,6 @@ export interface httpRequestInterceptors {
 
 // 定义一个新接口httpRequestConfig继承了AxiosRequestConfig的所有类型，在下面的对象中可以新增其他的类型
 export interface httpRequestConfig extends AxiosRequestConfig {
-  interceptors?: httpRequestInterceptors,
+  interceptors?: httpRequestInterceptors
   headers?: any
 }
